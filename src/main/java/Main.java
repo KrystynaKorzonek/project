b7162d2473371743c76359be147c012ea1cdee05
kortaa <adamkorta13@gmail.com>
2023-03-29 08:51:09 +0200

    Dodanie komunikatów błędów

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,31 +6,42 @@ public class Main {
         System.out.println("Wybierz figurę (S - kwadrat, C - koło)\nX - Zakończ");
         String input = scan.nextLine().toLowerCase();
         while (!input.equals("s") && !input.equals("c") && !input.equals("x")){
+            System.out.println("Nieprawidłowa wartość!");
+            System.out.println("Wybierz figurę (S - kwadrat, C - koło)\nX - Zakończ");
             input = scan.nextLine().toLowerCase();
         }
-        System.out.println("Wybierz cechę, którą podasz:");
-        Shape shape = null;
-        // TODO: remove repeated code
 
         if (input.equals("x")){
             return false;
         }
+        System.out.println("Wybierz cechę, którą podasz:");
+        Shape shape = null;
         if (input.equals("s")){
             System.out.println(Square.getMessageChoiceFeature());
             String feature = scan.nextLine().toLowerCase();
-            while (!Square.isFeatureCode(feature))
+            while (!Square.isFeatureCode(feature)) {
+                System.out.println("Nieprawidłowa wartość");
+                System.out.println("Wybierz cechę, którą podasz:");
+                System.out.println(Square.getMessageChoiceFeature());
                 feature = scan.nextLine().toLowerCase();
+            }
             System.out.println("Podaj jej wartość:");
             boolean done = false;
             double value;
-            while (!done){
-                value = scan.nextDouble();
+            String string_value;
+            while (!done) {
+                string_value = scan.nextLine();
                 try {
-                    shape = new Square(feature, value);
-                    done = true;
-                }
-                catch (IllegalArgumentException e){
-                    System.out.println(e.getMessage() + "\nSpróbuj jeszcze raz:");
+                    value = Double.parseDouble(string_value);
+
+                    try {
+                        shape = new Square(feature, value);
+                        done = true;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage() + "\nSpróbuj jeszcze raz:");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Błąd input nie jest liczbę!" + "\nSpróbuj jeszcze raz");
                 }
             }
 
@@ -38,19 +49,31 @@ public class Main {
         else{ //input == "c"
             System.out.println(Circle.getMessageChoiceFeature());
             String feature = scan.nextLine().toLowerCase();
-            while (!Circle.isFeatureCode(feature))
+            while (!Circle.isFeatureCode(feature)) {
+                System.out.println("Nieprawidłowa wartość");
+                System.out.println("Wybierz cechę, którą podasz:");
+                System.out.println(Circle.getMessageChoiceFeature());
                 feature = scan.nextLine().toLowerCase();
+            }
             System.out.println("Podaj jej wartość:");
             boolean done = false;
             double value;
+            String string_value;
             while (!done){
-                value = scan.nextDouble();
+                string_value = scan.nextLine();
                 try {
-                    shape = new Circle(feature, value);
-                    done = true;
+                    value = Double.parseDouble(string_value);
+
+                    try {
+                        shape = new Circle(feature, value);
+                        done = true;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage() + "\nSpróbuj jeszcze raz:");
+                    }
                 }
-                catch (IllegalArgumentException e){
-                    System.out.println(e.getMessage() + "\nSpróbuj jeszcze raz:");
+
+                catch(NumberFormatException e){
+                    System.out.println("Błąd input nie jest liczbę!" + "\nSpróbuj jeszcze raz");
                 }
             }
 
